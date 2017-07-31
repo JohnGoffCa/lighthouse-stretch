73c487a38358507f5550b61a4e838444243f28d3
John Goff <gygn.johngoff@gmail.com>
2017-07-31 08:36:47 -0400

    Initial beginning of telephone number exercise

---
+++ telephone/telephone.js
@@ -0,0 +1,13 @@
+const numberPairs = {
+  2: "ABC",
+  3: "DEF",
+  4: "GHI",
+  5: "JKL",
+  6: "MNO",
+  7: "PRS",
+  8: "TUV",
+  9: "WXY",
+}
+
+function addLetters(arr, number) {
+  let firstNum
